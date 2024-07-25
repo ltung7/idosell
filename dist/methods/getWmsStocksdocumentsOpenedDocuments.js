de67a2c754e552a827b5617423f731d0b7caeac9
Tomasz Le <tomasz.le@finnergroup.com>
2024-07-25 17:11:08 +0200

    Fixed get WMS dates

--- dist/methods/getWmsStocksdocumentsOpenedDocuments.js
+++ dist/methods/getWmsStocksdocumentsOpenedDocuments.js
@@ -4,7 +4,7 @@ import { dateRangeType, page } from "../helpers.js";
 export default (object) => {
     object.gate = { method: 'get', node: '/wms/stocksdocuments/openedDocuments' };
     object.custom = {
-        dates: dateRangeType({"nodeName":"dateObject","fromName":"dateBegin","toName":"dateEnd","typeName":"dateType","format":"YYYY-MM-DD HH:mm:ss","defaultType":"open"}),
+        dates: dateRangeType({"nodeName":"dateRange","fromName":"dateBegin","toName":"dateEnd","typeName":"dateType","format":"YYYY-MM-DD HH:mm:ss","defaultType":"open"}),
         page
     };
     return new Proxy(object, paramsProxy);
