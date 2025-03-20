513639e0ac421d96f0e34a8c1681f3954878d1e4
Tung Le <tomasz.le@finnergroup.com>
2025-03-20 13:50:05 +0100

    Fixed params nad getReturns.dates

--- dist/methods/getReturns.js
+++ dist/methods/getReturns.js
@@ -4,7 +4,7 @@ import { dateRangeType, page } from "../helpers.js";
 export default (object) => {
     object.gate = { method: 'get', node: '/returns/returns' };
     object.custom = {
-        dates: dateRangeType({"nodeName":"date","fromName":"date_begin","toName":"date_end","typeName":"dates_type","nested":"range","defaultType":"date_add"}),
+        dates: dateRangeType({"nodeName":"range","fromName":"date_begin","toName":"date_end","typeName":"dates_type","defaultType":"date_add"}),
         page
     };
     object.snakeCase = true;
